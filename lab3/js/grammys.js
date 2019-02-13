474957a0a033f9d9d4b1a0f06a150aa5fea34159
Eduardo Hernández <eddyhdzg@gmail.com>
2019-02-13 01:22:04 -0600

    Lab3

---
+++ lab3/js/grammys.js
@@ -0,0 +1,70 @@
+$.ajax({
+  url: "data/grammys.json",
+  type: "GET",
+  dataType: "json",
+  success: function(data) {
+    let new_html = "";
+
+    for (let i = 0; i < data.fields.length; i++) {
+      new_html += `
+        <option value="${data.fields[i].field_id - 1}">
+          ${data.fields[i].field}
+        </option>
+      `;
+    }
+
+    $("#category_types").append(new_html);
+    loadNominees();
+  },
+  error: function(error_msg) {
+    console.log(error_msg);
+  }
+});
+
+function loadNominees() {
+  $.ajax({
+    url: "data/grammys.json",
+    type: "GET",
+    dataType: "json",
+    success: function(data) {
+      $("#category_types").on("change", function(event) {
+        let id = $(this).val();
+        if (id !== "-1") {
+          let selectedField = data.fields[id];
+
+          let new_html = "";
+
+          new_html += `<h2>${selectedField.field}</h2>`;
+
+          // Checks if the description exist
+          if (selectedField.description) {
+            new_html += `<p>${selectedField.description}</p>`;
+          }
+
+          for (let i = 0; i < selectedField.categories.length; i++) {
+            new_html += `<h3>${selectedField.categories[i].category_name} 
+          </h3><ul>`;
+            for (
+              let j = 0;
+              j < selectedField.categories[i].nominees.length;
+              j++
+            ) {
+              new_html += `<li><strong>${
+                selectedField.categories[i].nominees[j].nominee
+              }</strong>  
+            <p>${selectedField.categories[i].nominees[j].artist}</p>
+            <p>${selectedField.categories[i].nominees[j].info}</p>
+            </li>`;
+            }
+            new_html += `</ul>`;
+          }
+
+          $("#nominees_section").html(new_html);
+        }
+      });
+    },
+    error: function(error_msg) {
+      console.log(error_msg);
+    }
+  });
+}
